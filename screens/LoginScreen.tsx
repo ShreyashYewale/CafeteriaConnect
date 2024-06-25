df8d3ec0ba3e3c4c6802cb91ab4a060e784f3683
Pro-Sid <sodhasiddarth@gmail.com>
2024-06-25 21:38:10 +0530

    form validation and api call

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,51 +1,86 @@
-import {View, TextInput, Button} from 'react-native';
-import React, {useCallback, useEffect} from 'react';
-import {useForm} from 'react-hook-form';
+import {View, TextInput, Button, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {buttonClass, inputClass} from '../css-util';
 import {RootStackParamList} from '../App';
 
 const LoginScreen = () => {
-  const {register, handleSubmit, setValue} = useForm();
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
-  const onSubmit = useCallback((formData: any) => {
-    console.log(formData, formData.username);
-    navigation.navigate('Home');
-  }, []);
-
-  const onChangeField = useCallback(
-    (name: string) => (text: string) => {
-      setValue(name, text);
-    },
-    [],
-  );
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [errors, setErrors] = useState<{[key: string]: string}>({});
+  const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
-    register('username');
-    register('password');
-  }, [register]);
+    validateForm();
+  }, [name, email]);
+
+  const validateForm = () => {
+    let errors: any = {};
+
+    // Validate name field
+    if (!name) {
+      errors.name = 'Name is required.';
+    }
+
+    // Validate email field
+    if (!email) {
+      errors.email = 'Email is required.';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email is invalid.';
+    }
+
+    // Set the errors and update form validity
+    setErrors(errors);
+    setIsFormValid(Object.keys(errors).length === 0);
+  };
+
+  const handleLogin = async () => {
+    if (isFormValid) {
+      // Form is valid, perform the submission logic
+      try {
+        await fetch('http://localhost:8000/api/signin', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({name, email}),
+        });
+        navigation.navigate('Home');
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      // Form is invalid, display error messages
+      console.error('Form has errors. Please correct them.');
+    }
+  };
 
   return (
-    <View className="flex-1 items-center justify-center p-2 space-y-4">
+    <View className="flex-1 p-2 justify-center space-y-4">
       <TextInput
-        className={inputClass}
-        autoComplete="username"
-        keyboardType="default"
-        textContentType="username"
-        placeholder="Username"
-        onChangeText={onChangeField('username')}
+        className="border border-gray-300 rounded-lg"
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
       />
       <TextInput
-        className={inputClass}
-        secureTextEntry
-        autoComplete="password"
-        placeholder="Password"
-        onChangeText={onChangeField('password')}
+        className="border border-gray-300 rounded-lg"
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
       />
-      <View className={buttonClass}>
-        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      <View className="bg-green-400 rounded-lg">
+        <Button title="Submit" disabled={!isFormValid} onPress={handleLogin} />
       </View>
+
+      {/* Display error messages */}
+      {Object.values(errors).map((error, index) => (
+        <Text key={index} className="text-red-700">
+          {error}
+        </Text>
+      ))}
     </View>
   );
 };
--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,64 +1,101 @@
-import {View, TextInput, Button} from 'react-native';
-import React, {useCallback, useEffect} from 'react';
-import {useForm} from 'react-hook-form';
-import {inputClass} from '../css-util';
+import {View, TextInput, Button, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParamList} from '../App';
 
 const RegisterScreen = () => {
-  const {register, handleSubmit, setValue} = useForm();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
-  const onSubmit = useCallback((formData: any) => {
-    console.log(formData, formData.username);
-  }, []);
-
-  const onChangeField = useCallback(
-    (name: string) => (text: string) => {
-      setValue(name, text);
-    },
-    [],
-  );
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<{[key: string]: string}>({});
+  const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
-    register('username');
-    register('email');
-    register('password');
-    register('confirm password');
-  }, [register]);
+    validateForm();
+  }, [name, email, password]);
+
+  const validateForm = () => {
+    let errors: any = {};
+
+    // Validate name field
+    if (!name) {
+      errors.name = 'Name is required.';
+    }
+
+    // Validate email field
+    if (!email) {
+      errors.email = 'Email is required.';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email is invalid.';
+    }
+
+    // Validate password field
+    if (!password) {
+      errors.password = 'Password is required.';
+    } else if (password.length < 3) {
+      errors.password = 'Password must be at least 3 characters.';
+    }
+
+    // Set the errors and update form validity
+    setErrors(errors);
+    setIsFormValid(Object.keys(errors).length === 0);
+  };
+
+  const handleSubmit = async () => {
+    if (isFormValid) {
+      // Form is valid, perform the submission logic
+      try {
+        const res = await fetch('http://localhost:8000/api/signup', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({name, email, password}),
+        });
+        navigation.navigate('Home');
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      // Form is invalid, display error messages
+      console.error('Form has errors. Please correct them.');
+    }
+  };
 
   return (
-    <View className="flex-1 items-center justify-center  p-2 space-y-4">
+    <View className="flex-1 p-2 justify-center space-y-4">
       <TextInput
-        className={inputClass}
-        autoComplete="username"
-        keyboardType="default"
-        textContentType="username"
-        placeholder="Username"
-        onChangeText={onChangeField('username')}
+        className="border border-gray-300 rounded-lg"
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
       />
       <TextInput
-        className={inputClass}
-        autoComplete="email"
-        keyboardType="email-address"
-        textContentType="emailAddress"
+        className="border border-gray-300 rounded-lg"
         placeholder="Email"
-        onChangeText={onChangeField('email')}
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
-        className={inputClass}
-        secureTextEntry
-        autoComplete="password"
+        className="border border-gray-300 rounded-lg"
         placeholder="Password"
-        onChangeText={onChangeField('password')}
-      />
-      <TextInput
-        className={inputClass}
+        value={password}
+        onChangeText={setPassword}
         secureTextEntry
-        autoComplete="password"
-        placeholder="Confirm Password"
-        onChangeText={onChangeField('confirm password')}
       />
-      <View className="mt-2">
-        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      <View className="bg-green-400 rounded-lg">
+        <Button title="Submit" disabled={!isFormValid} onPress={handleSubmit} />
       </View>
+
+      {/* Display error messages */}
+      {Object.values(errors).map((error, index) => (
+        <Text key={index} className="text-red-700">
+          {error}
+        </Text>
+      ))}
     </View>
   );
 };
